f8bac7cff1234c6992127208f8d92f870a529847
Andrew Yu <igotyuu421@gmail.com>
2021-03-23 13:10:39 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// Assignment Code
+//DO NOT CHANGE THIS CODE
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+  //TODO: your code here
+
+}
+
+// Write password to the #password input
+/* DO NOT CHANGE THIS CODE!  This function is going to call your generatePassword() function and whatever is returned from that function will be put onto the page */
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+// DO NOT CHANGE THIS CODE
+generateBtn.addEventListener("click", writePassword);
