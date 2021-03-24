13f0a3447fe763d112fa70fee4bb4e04e0d9444d
Andrew Yu <igotyuu421@gmail.com>
2021-03-24 15:37:32 -0700

    java additions

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 // Assignment Code
+var upperCase =true;
+var lowerCase =true;
+var specialCharacter =true;
+var numericalValue =true;
+var lengthOfPassword = 0;
+var lowerCaseString = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberString = "0123456789";
+var symbolsString ="!@#$%^&*(_{}[]|?><.,";
+var characterHolder= "";
+
+
 //DO NOT CHANGE THIS CODE
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword(){
   //TODO: your code here
+
+lengthOfPassword = prompt(
+  "How long would you like your password?", "Between 8-128 characters");
+   upperCase= confirm('Would you like UpperCase Letters included?');
+   lowerCase= confirm('Would you like Lowercase Letters included?');
+   specialCharacter= confirm('Would you like Special characters included?');
+   numericalValue= confirm('Would you like Numbers included?');
+
+if(lengthOfPassword. lengthOfPassword<8 ||lengthOfPassword>128){
+  return "Enter a valid password length";
+}else if (!upperCase&& !lowerCase&& !specialCharacter&& !numericalValue){
+  return "Select at least one type of character for your password";
+}else{
+  if(upperCase){
+    characterHolder += upperCaseString;
+  }
+  if(lowerCase){
+    characterHolder += lowerCaseString;
+  }
+  if(numericalValue){
+    characterHolder += numberString;
+  }
+}
 
 }
 
